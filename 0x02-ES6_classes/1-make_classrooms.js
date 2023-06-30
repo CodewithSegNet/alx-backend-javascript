b27667cc8aae5c708778eefe567c710adecb6c40
segunUb18 <eeomo@yahoo.com>
2023-06-30 16:12:22 +0100

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+//eslint-disable-next-line no-unused-vars
+function initializeRooms() {
+ return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+}
+
+export default initializeRooms;
