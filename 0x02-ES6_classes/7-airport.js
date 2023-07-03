da8718b431944f14c3534eab0026ac57d9ca21bf
segunUb18 <eeomo@yahoo.com>
2023-07-03 12:05:32 +0100

    fix

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,11 +1,9 @@
 export default class Airport {
   constructor(name, code) {
-    // set _parameter properties to the specific Airport class
     this._name = name;
     this._code = code;
   }
 
-  // define a getter for constructor parameter
   get name() {
     return this._name;
   }
@@ -14,7 +12,6 @@ export default class Airport {
     return this._code;
   }
 
-  // return the default string of code
   toString() {
     return this._code;
   }
