d99f4529e0ef7d0625939e03d0d8621e0ce157c7
segunUb18 <eeomo@yahoo.com>
2023-07-03 12:13:50 +0100

    fix 6-sky_high

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,29 +2,19 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    // call constructor of super class (building) using super()
     super(sqft);
-    // set the _floors property specific to the SkyHighBuilding class
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  // define a getter for floors property
   get floors() {
     return this._floors;
   }
-  // define a setter for floors property
+
   set floors(value) {
-  return this._floors = value;
+    this._floors = value;
   }
 
-  // define a getter for sqft property
-  get sqft() {
-    return this._sqft;
-  }
-
-  // Implement the evacuationWarningMessage method required by the Building superclass
   evacuationWarningMessage() {
-  // Return a message specific to the SkyHighBuilding class
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
