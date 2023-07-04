cb6a09a58f3f3fa8b67661c395d2d67a8b8ff936
segunUb18 <eeomo@yahoo.com>
2023-07-04 12:32:32 +0100

    :pencil2: Fix author
    change name

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/codewithsegnet>
+ * @author Olusegun Emmanuel <https://github.com/codewithsegnet>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
