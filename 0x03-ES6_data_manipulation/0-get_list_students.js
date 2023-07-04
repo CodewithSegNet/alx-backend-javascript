bb0f5f91e1c689f2e3df965ebce523a747cb89d0
segunUb18 <eeomo@yahoo.com>
2023-07-04 13:41:43 +0100

    :recycle: get_list_students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,15 @@
+class Student {
+  constructor(id, firstName, location) {
+    this.id = id;
+    this.firstName = firstName;
+    this.location = location;
+  }
+}
+
 export default function getListStudents() {
-  return [{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-	  {id: 2, firstName: 'James', location: 'Columbia'},
-	  {id: 5, firstName: 'Serena', location: 'San Francisco'}]
+  return [
+    new Student(1, 'Guillaume', 'San Francisco'),
+    new Student(2, 'James', 'Columbia'),
+    new Student(5, 'Serena', 'San Francisco'),
+  ];
 }
