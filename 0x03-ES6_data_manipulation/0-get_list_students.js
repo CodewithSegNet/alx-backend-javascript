0fec57074868a549e1d5dd2a5d7d7aa2fcdc298f
segunUb18 <eeomo@yahoo.com>
2023-07-04 13:01:36 +0100

    :bug: fix task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,9 +4,15 @@
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
-  return [
+  const studentData = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return studentData.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
 }
