1c22920cd6d7f171290a96323e0a660b5ddebe45
segunUb18 <eeomo@yahoo.com>
2023-07-04 13:15:05 +0100

    :fire: remove code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,19 @@
-/**
- * Retrieves a list of students.
- * @author Olusegun Emmanuel <https://github.com/codewithsegnet>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
-  const studentData = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  return [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
   ];
-
-  return studentData.map((student) => ({
-    id: student.id,
-    firstName: student.firstName,
-    location: student.location,
-  }));
 }
