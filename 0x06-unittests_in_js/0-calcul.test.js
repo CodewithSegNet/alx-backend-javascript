02b8cfff198f61860b108dc93a771e847b577927
segunUb18 <eeomo@yahoo.com>
2023-08-25 14:34:30 +0100

    :bug: Fix for test run
    fix for test run tast 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,15 +6,23 @@ describe('calculateNumber', () => {
 		assert.strictEqual(calculateNumber(1, 3), 4);
 	});
 
-	it('floating point whole numbers', () => {
+	it('rounding up b\'s floating point fractional numbers', () => {
 		assert.strictEqual(calculateNumber(1, 3.7), 5);
 	});
 
-	it('rounding up b\'s floating point fractional number', () => {
-		assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+	it('rounding both a and b\'s floating point fractional number', () => {
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
 	});
 
-	it('rounding up b\'s floating point fractional numbers', () => {
+	it('rounding up a and b\'s floating point fractional numbers', () => {
 		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+	});
+
+	it('rounding down a\'s floating point fractional number', () => {
+		assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+	});
+
+	it('rounding down b\'s floating point fractional numbers', () => {
+		assert.strictEqual(calculateNumber(1.0, 2.4), 3);
 	});
 });
