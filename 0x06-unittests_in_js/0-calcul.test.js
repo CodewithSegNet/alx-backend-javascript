9f4e5a4c1dd0c147206d3d9187fe6a30ebc99135
segunUb18 <eeomo@yahoo.com>
2023-08-25 14:12:51 +0100

    :bug: Fix for test case
    fix for test case

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,8 +4,17 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', () => {
 	it('should return the sum of rounded numbers', () => {
 		assert.strictEqual(calculateNumber(1, 3), 4);
+	});
+
+	it('floating point whole numbers', () => {
 		assert.strictEqual(calculateNumber(1, 3.7), 5);
-		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+	});
+
+	it('rounding up b\'s floating point fractional number', () => {
+		assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+	});
+
+	it('rounding up b\'s floating point fractional numbers', () => {
 		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
 	});
 });
